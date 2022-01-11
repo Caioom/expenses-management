40690b01ce9d031b764b26185ab921bfa5880481
caioom <caio.ds.2003@gmail.com>
2022-01-11 08:08:46 -0300

    props and dynamic data using JSX

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import ExpenseItem from './components/ExpenseItem'
+import Expenses from './components/Expenses'
 
 function App() {
+  const expenses = [
+    {
+      title: 'Car Insurance',
+      amount: 298.78,
+      date: new Date(2021, 2, 28)
+    },
+    {
+      title: 'Car Fix',
+      amount: 88.78,
+      date: new Date(2021, 2, 28)
+    },
+    {
+      title: 'Car Wheels',
+      amount: 328.78,
+      date: new Date(2021, 2, 28)
+    },
+    {
+      title: 'Coffee',
+      amount: 38.78,
+      date: new Date(2021, 2, 20)
+    }
+  ]
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <Expenses items={expenses}/>
     </div>
   );
 }
