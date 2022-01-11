3e0acc687c68e3558768e1feb4a26c36ace27850
Caio Oliveira de Mello1 <caio.mello@ibm.com>
2022-01-11 08:51:51 -0300

    using children props to create wrapper components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,16 @@
 import './ExpenseItem.css'
-
+import Card from './Card'
 import ExpenseDate from './ExpenseDate'
 
 function ExpenseItem(props) {
     return (
-        <div className="expense-item">
+        <Card className="expense-item"> 
             <div className="expense-item__description">
                 <ExpenseDate date={props.date}></ExpenseDate>
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
-        </div>
+        </Card>
     )
 }
 
