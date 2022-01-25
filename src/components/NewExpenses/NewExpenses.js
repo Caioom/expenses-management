c6d30a43e285bf83a5555a1b155d3b962a42dc68
caioom <caio.ds.2003@gmail.com>
2022-01-25 11:52:03 -0300

    adding cancel button

--- src/components/NewExpenses/NewExpenses.js
+++ src/components/NewExpenses/NewExpenses.js
@@ -1,15 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './NewExpense.css'
 import ExpenseForm from './ExpenseForm'
 
 const NewExpenses = (props) => {
+    const [addNewExpenseClicked, setAddNewExpenseClicked] = useState(false)
     const saveNewExpenseHandler = (expense) => {
         props.onAddNewExpense(expense)
+        setAddNewExpenseClicked(false)
+    }
+
+    const addNewExpenseHandler = () => {
+        setAddNewExpenseClicked(() => !addNewExpenseClicked)
+    }
+
+    const cancelExpenseEditingHandler = () => {
+        setAddNewExpenseClicked(false)
+    }
+
+    let addNewExpenseContent = <button onClick={addNewExpenseHandler}>Add new expense</button>
+    if(addNewExpenseClicked) {
+        addNewExpenseContent = <ExpenseForm onCancel={cancelExpenseEditingHandler} onSaveNewExpense={saveNewExpenseHandler}/>
     }
 
     return (
         <div className="new-expense">
-            <ExpenseForm onSaveNewExpense={saveNewExpenseHandler}/>
+            {addNewExpenseContent} 
         </div>
     )
 }
